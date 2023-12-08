c01fb1d68024f84dba3fd64d18b758fac5afe345
Shadan K <shadankalam@gmail.com>
2023-12-08 17:49:11 +0530

    formatted routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -9,31 +9,55 @@ module.exports = app => {
     var router = require('express').Router();
     
     // User Routes
-    router.post('/user/add', authenticator.authenticate, authorize.authorize(['admin']), formValidator.user_validation, user.addNew);
+    router.post('/user/add', authenticator.authenticate, 
+        authorize.authorize(['admin']), 
+        formValidator.user_validation, user.addNew);
 
-    router.get('/user/fetch', authenticator.authenticate,authorize.authorize(['admin']), formValidator.user_fetch_validation, user.fetch);
+    router.get('/user/fetch', 
+        authenticator.authenticate,authorize.authorize(['admin']), 
+        formValidator.user_fetch_validation, user.fetch);
 
-    router.put('/user/update',authenticator.authenticate,authorize.authorize(['admin']), user.update)
+    router.put('/user/update', 
+        authenticator.authenticate, 
+        authorize.authorize(['admin']), user.update)
     
-    router.delete('/user/delete',authenticator.authenticate, user.delete)
+    router.delete('/user/delete',
+        authenticator.authenticate, 
+        authorize.authorize(['admin']), user.delete)
 
     //Invoice Routes
 
-    router.post('/invoice/add', authenticator.authenticate, authorize.authorize(['admin', 'receiver']), formValidator.invoice_validation, invoice.add)
+    router.post('/invoice/add', 
+        authenticator.authenticate, 
+        authorize.authorize(['admin', 'receiver']),
+        formValidator.invoice_validation, invoice.add)
 
-    router.get('/invoice/fetch', authenticator.authenticate, authorize.authorize(['admin', 'receiver']), invoice.fetch)
+    router.get('/invoice/fetch', 
+        authenticator.authenticate, 
+        authorize.authorize(['admin', 'receiver']), invoice.fetch)
 
-    router.put('/invoice/updateStatus', authenticator.authenticate, authorize.authorize(['admin']), invoice.updateStatus)
+    router.put('/invoice/updateStatus',
+        authenticator.authenticate, 
+        authorize.authorize(['admin']), invoice.updateStatus)
 
-    router.delete('/invoice/delete', authenticator.authenticate,authorize.authorize(['admin']), invoice.delete)
+    router.delete('/invoice/delete', 
+        authenticator.authenticate, 
+        authorize.authorize(['admin']), invoice.delete)
 
     //Payment Routes
 
-    router.post('/payment/add', authenticator.authenticate,authorize.authorize(['admin', 'payer']), formValidator.payment_validation, payment.add)
+    router.post('/payment/add', 
+        authenticator.authenticate,
+        authorize.authorize(['admin', 'payer']), 
+        formValidator.payment_validation, payment.add)
 
-    router.get('/payment/fetch',authenticator.authenticate, authorize.authorize(['admin']), payment.fetch)
+    router.get('/payment/fetch', 
+        authenticator.authenticate, 
+        authorize.authorize(['admin']), payment.fetch)
 
-    router.delete('/payment/delete', authenticator.authenticate, authorize.authorize(['admin']), payment.delete)
+    router.delete('/payment/delete', 
+        authenticator.authenticate, 
+        authorize.authorize(['admin']), payment.delete)
 
     app.use('/invoice-system', authenticator.authenticate,router);
 };
